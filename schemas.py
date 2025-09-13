837dd6bbc93f4adf1fb5f19e61fd7ea1edc0037e
Pawan Uikey <pawanuikey690@gmail.com>
2025-09-13 08:20:56 +0530

    required files

---
+++ schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel ,Field
+from typing import List ,Optional
+from datetime import date
+
+
+class EmployeeBase(BaseModel):
+    name: str
+    department:str
+    salary:int
+    joining_date:date
+    skills:List[str]
+
+
+class CreateEmployee(EmployeeBase):
+    employee.id: str = Field(...)
+
+
+class UpdateEmployee(BaseModel):
+    name: Optional[str]
+    department: Optional[str]
+    salary: Optional[int]
+    joining_date: Optional[date]
+    skills: Optional[List[str]]
+
+
